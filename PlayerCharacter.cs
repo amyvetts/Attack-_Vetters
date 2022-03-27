7cb7a2c4968e7e8a55bf61db1a5ef8f4c5b17f6c
amy vetters <amyvetters@gmail.com>
2022-03-27 16:06:29 -0400

    Add project files.

---
+++ PlayerCharacter.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Linq;
+
+class PlayerCharacter
+{
+
+    /* static field */
+    private static Random RNG = new Random();
+
+    /* Member Data "fields" */
+    private int[] attributes;
+
+    /* Member Data "properties" */
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Health { get; private set; }
+    public int Strength
+    {
+        get
+        {
+            return attributes[0];
+        }
+
+        private set
+        {
+            attributes[0] = value;
+        }
+    }
+
+    public int Dexterity
+    {
+        get
+        {
+            return attributes[1];
+        }
+
+        private set
+        {
+            attributes[1] = value;
+        }
+    }
+
+    public int Constitution
+    {
+        get
+        {
+            return attributes[2];
+        }
+
+        private set
+        {
+            attributes[2] = value;
+        }
+    }
+
+    public int Intelligence
+    {
+        get
+        {
+            return attributes[3];
+        }
+
+        private set
+        {
+            attributes[3] = value;
+        }
+    }
+
+    public int Wisdom
+    {
+        get
+        {
+            return attributes[4];
+        }
+
+        private set
+        {
+            attributes[4] = value;
+        }
+    }
+
+    public int Charisma
+    {
+        get
+        {
+            return attributes[5];
+        }
+
+        private set
+        {
+            attributes[5] = value;
+        }
+    }
+
+    // constructors
+
+    //default constructor
+    public PlayerCharacter()
+    {
+        this.Name = "";    // default name
+        this.Age = RNG.Next(80) + 11;   // 11-90 range default
+        this.attributes = new int[6];
+        for (int attribute = 0; attribute < this.attributes.Length; attribute++)
+        {
+            int[] dice = new int[4];
+            for (int die = 0; die < dice.Length; die++)
+            {
+                dice[die] = RNG.Next(6) + 1;
+            }
+            this.attributes[attribute] = dice.Sum() - dice.Min();
+        }
+
+        this.Health = 10 + (this.Constitution - 10) / 2;
+    }
+
+    public PlayerCharacter(string Name) : this()
+    {
+        this.Name = Name;
+    }
+
+    public PlayerCharacter(string Name,
+                           int s,
+                           int d,
+                           int cn,
+                           int i,
+                           int w,
+                           int ch) : this(Name)
+    {
+        this.Strength = s;
+        this.Dexterity = d;
+        this.Constitution = cn;
+        this.Intelligence = i;
+        this.Wisdom = w;
+        this.Charisma = ch;
+
+        this.Health = 10 + (this.Constitution - 10) / 2;
+    }
+
+    // methods
+
+    /*
+    In C#, we will use properties instead of accessor
+    and mutator methods.
+
+    //accessor methods (getters)
+    public int getHealth() {
+      return Health;
+    }
+
+    //mutator methods (setters)
+    public void setHealth(int newHealth) {
+      Health = newHealth;
+    }
+    */
+
+    public string CharacterSheet()
+    {
+        string sheet = "";
+        sheet += "===================================\n";
+        sheet += $"Name: {this.Name}\t\tAge: {this.Age}\n";  // sheet +=  is same as   sheet = sheet +
+        sheet += "-----------------------------------\n";
+        sheet += $"\tSTR: {this.Strength}\n";
+        sheet += $"\tDEX: {this.Dexterity}\n";
+        sheet += $"\tCON: {this.Constitution}\n";
+        sheet += $"\tINT: {this.Intelligence}\n";
+        sheet += $"\tWIS: {this.Wisdom}\n";
+        sheet += $"\tCHR: {this.Charisma}\n";
+        sheet += "-----------------------------------\n";
+        sheet += $"Health: {this.Health}\n";
+        sheet += "===================================\n";
+        return sheet;
+    }
+
+    public bool isAlive()
+    {
+        return (Health > 0);
+    }
+
+    public string Tackle(PlayerCharacter defender)
+    {
+        string combatDescription = "";
+        // probability to hit
+        if (RNG.Next(20) + 1 > defender.Dexterity)
+        {
+            //you hit!
+            // randomize the damage 
+            int damageDone = (RNG.Next(4) + 1 + (this.Strength - 10) / 2);
+            damageDone = Math.Max(damageDone, 1);
+            combatDescription = $"{this.Name}[{this.Health}] tackles {defender.Name}[{defender.Health}] for {damageDone} damage!";
+            defender.Health = defender.Health - damageDone;
+        }
+        else
+        {
+            //you miss!
+            combatDescription = $"{this.Name}[{this.Health}] tries to tackle {defender.Name}[{defender.Health}] and misses!";
+        }
+        // give the returned message more detail
+        return combatDescription;
+    }
+    public string Stone(PlayerCharacter defender)
+    {
+        string combatDesc = "";
+        if(RNG.Next(20) + 1 > defender.Intelligence)
+        {
+            //hit
+            int damage = (RNG.Next(4) + 1+ (this.Strength - 10) / 2) ;
+            damage = Math.Max(damage, 3);
+            combatDesc = $"{this.Name}[{this.Health}] stones {defender.Name}[{defender.Health}] for {damage} damage!";
+            defender.Health = defender.Health - damage;
+        }
+        else
+        {
+            combatDesc = $"{this.Name}[{this.Health}] tries to stone {defender.Name}[{defender.Health}] and misses!";
+        }
+        return combatDesc;
+    }
+
+    public string Pummel(PlayerCharacter defender)
+    {
+        string combatDescript = "";
+        if (RNG.Next(20) + 1 > defender.Wisdom)
+        {
+            //hit
+            int damage = (RNG.Next(4) + 1 + (this.Strength - 10) / 2);
+            damage = Math.Max(damage, 4);
+            combatDescript = $"{this.Name}[{this.Health}] pummels {defender.Name}[{defender.Health}] for {damage} damage!";
+            defender.Health = defender.Health - damage;
+        }
+        else
+        {
+            combatDescript = $"{this.Name}[{this.Health}] tries to pummel {defender.Name}[{defender.Health}] and misses!";
+        }
+        return combatDescript;
+    }
+    
+    
+}
---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+class MainClass
+{
+    private static Random RNG = new Random();
+
+    public static void Main(string[] args)
+    {
+    /*
+     * Generate two Characters P1 and P2
+     */
+    PlayerCharacter P1 = new PlayerCharacter("Zavier", 18, 11, 17, 15, 14, 13);
+        PlayerCharacter P2 = new PlayerCharacter("Chris");
+
+        /*
+         * Print out their stats
+         */
+
+        System.Console.WriteLine(P1.CharacterSheet());
+        System.Console.WriteLine(P2.CharacterSheet());
+
+        /*
+        * Menu-System for attacking one another
+        * Fight to the mostly dead
+        */
+
+        System.Console.WriteLine($"Before the fight, {P1.Name} has {P1.Health} HP, and " +
+                                  $"{P2.Name} has {P2.Health} HP.");
+        /*while (P1.isAlive() && P2.isAlive())
+        {
+            //System.Console.WriteLine("Menu of Choices");
+            System.Console.WriteLine(P1.Tackle(P2));
+            System.Console.WriteLine(P2.Tackle(P1));
+        }
+
+        if (!P1.isAlive())
+        {
+            System.Console.WriteLine(P1.Name + " RIP!!!");
+        }
+        if (!P2.isAlive())
+        {
+            System.Console.WriteLine(P2.Name + " RIP!!!");
+        }*/
+
+        
+        while (P1.isAlive() && P2.isAlive())
+        {
+            char attack = '0';
+            string label = "";
+            string[] playerAttack = new string[] {"Tackle", "Stone", "Pummel"};
+            System.Console.WriteLine("Menu of Choices:");
+            System.Console.WriteLine("1: Tackle");
+            System.Console.WriteLine("2: Stone");
+            System.Console.WriteLine("3: Pummel");
+            System.Console.Write("Choose your method of attack: ");
+            while (attack < '1' || attack > '3')
+            {
+                attack = (System.Console.ReadLine()).ToCharArray()[0];
+                int index = RNG.Next(playerAttack.Length);
+
+
+
+                if (attack == '1' && index == 0)
+                {
+                    label = "Tackle";
+                    System.Console.WriteLine($"You chose to {label} your opponent.");
+                    System.Console.WriteLine($"Your opponent chose to {playerAttack[index]} you.");
+                    System.Console.WriteLine(P1.Tackle(P2));
+                    System.Console.WriteLine(P2.Tackle(P1));
+                }
+                else if (attack == '1' && index == 1)
+                {
+                    label = "Tackle";
+                    System.Console.WriteLine($"You chose to {label} your opponent.");
+                    System.Console.WriteLine($"Your opponent chose to {playerAttack[index]} you.");
+                    System.Console.WriteLine(P1.Tackle(P2));
+                    System.Console.WriteLine(P2.Stone(P1));
+                }
+                else if (attack == '1' && index == 2)
+                {
+                    label = "Tackle";
+                    System.Console.WriteLine($"You chose to {label} your opponent.");
+                    System.Console.WriteLine($"Your opponent chose to {playerAttack[index]} you.");
+                    System.Console.WriteLine(P1.Tackle(P2));
+                    System.Console.WriteLine(P2.Pummel(P1));
+                }
+                else if (attack == '2' && index == 0)
+                {
+                    label = "Stone";
+                    System.Console.WriteLine($"You chose to {label} your opponent.");
+                    System.Console.WriteLine($"Your opponent chose to {playerAttack[index]} you.");
+                    System.Console.WriteLine(P1.Stone(P2));
+                    System.Console.WriteLine(P2.Tackle(P1));
+
+                }
+                else if (attack == '2' && index == 1)
+                {
+                    label = "Stone";
+                    System.Console.WriteLine($"You chose to {label} your opponent.");
+                    System.Console.WriteLine($"Your opponent chose to {playerAttack[index]} you.");
+                    System.Console.WriteLine(P1.Stone(P2));
+                    System.Console.WriteLine(P2.Stone(P1));
+                }
+                else if (attack == '2' && index == 2)
+                {
+                    label = "Stone";
+                    System.Console.WriteLine($"You chose to {label} your opponent.");
+                    System.Console.WriteLine($"Your opponent chose to {playerAttack[index]} you.");
+                    System.Console.WriteLine(P1.Stone(P2));
+                    System.Console.WriteLine(P2.Pummel(P1));
+                }
+                else if (attack == '3' && index == 0)
+                {
+                    label = "Pummel";
+                    System.Console.WriteLine($"You chose to {label} your opponent.");
+                    System.Console.WriteLine($"Your opponent chose to {playerAttack[index]} you.");
+                    System.Console.WriteLine(P1.Pummel(P2));
+                    System.Console.WriteLine(P2.Tackle(P1));
+                }
+                else if (attack == '3' && index == 1)
+                {
+                    label = "Tackle";
+                    System.Console.WriteLine($"You chose to {label} your opponent.");
+                    System.Console.WriteLine($"Your opponent chose to {playerAttack[index]} you.");
+                    System.Console.WriteLine(P1.Pummel(P2));
+                    System.Console.WriteLine(P2.Stone(P1));
+                }
+                else
+                {
+                    label = "Pummel";
+                    System.Console.WriteLine($"You chose to {label} your opponent.");
+                    System.Console.WriteLine($"Your opponent chose to {playerAttack[index]} you.");
+                    System.Console.WriteLine(P1.Pummel(P2));
+                    System.Console.WriteLine(P2.Pummel(P1));
+                }
+            }
+        }
+
+        if (!P1.isAlive())
+        {
+            System.Console.WriteLine(P1.Name + " RIP!!!");
+        }
+        if (!P2.isAlive())
+        {
+            System.Console.WriteLine(P2.Name + " RIP!!!");
+        }
+
+
+    }
+
+}
